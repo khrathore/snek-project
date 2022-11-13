5ee930ef8dcbe2774d3e87d1589caee7f6969333
khrathore <44534747+khrathore@users.noreply.github.com>
2022-11-13 16:56:01 -0500

    Create terpplanner.py

---
+++ terpplanner.py
@@ -0,0 +1,40 @@
+'''
+Terp Planner: helps a student org plan their event on campus
+
+Project Concept (MUST HAVE 10 DISTINCT):
+    Conditional expressions: Event planning continues if the event is approved
+    Optional parameters and/or use of keyword arguments: To set default values of budget if budget is not set
+    F-strings: Maybe at the end of each selection, to say that $ ___ from your budget is left to spend
+    With statements: Create a text file that summarizes the event(s) that was planned
+    The ArgumentParser class: sequence unpacking
+    Set operations on sets or frozensets: Create a set of places on campus, Check to see if the location entered is in the set
+    Comprehensions or generator expressions: Allow the student to pick a location on campus and comprehend whether the
+place they picked is on campus
+    Custom list sorting with a key function (possibly a lambda expression): Food - sort the restaurants: vegetarian, vegan, and others
+    Using super():
+    Regular expressions:
+    Magic methods other than __init__(): A possible magic method subtract to work with the budget tracker
+    Concatenating, merging, filtering, or performing groupby operations on Pandas:
+    DataFrames:
+    Visualizing data with pyplot or seaborn:
+
+class Terp_Plans
+    Attributes:
+        name()
+        budget()
+        location() – can have a list and the user has to pick a location from that list
+        date()
+        time()
+        food()
+    Methods:
+        __init__()
+        budget_tracker(float)
+        create_event
+        event_approval
+        prices
+        find_food
+
+main()
+ArgParser
+if __name__ == "__main__"
+'''
