2df1eb30b79b2c3ac53057472abbc872462048d7
Sandra Aching <sakathia01@gmail.com>
2022-11-14 16:16:08 -0500

    Update terpplanner.py

--- terpplanner.py
+++ terpplanner.py
@@ -1,38 +1,46 @@
 '''
 Terp Planner: helps a student org plan their event on campus
 
-Project Concept (MUST HAVE 10 DISTINCT):
-    Conditional expressions: Event planning continues if the event is approved
-    Optional parameters and/or use of keyword arguments: To set default values of budget if budget is not set
-    F-strings: Maybe at the end of each selection, to say that $ ___ from your budget is left to spend
-    With statements: Create a text file that summarizes the event(s) that was planned
-    The ArgumentParser class: sequence unpacking
-    Set operations on sets or frozensets: Create a set of places on campus, Check to see if the location entered is in the set
-    Comprehensions or generator expressions: Allow the student to pick a location on campus and comprehend whether the
-place they picked is on campus
-    Custom list sorting with a key function (possibly a lambda expression): Food - sort the restaurants: vegetarian, vegan, and others
-    Using super():
-    Regular expressions:
-    Magic methods other than __init__(): A possible magic method subtract to work with the budget tracker
-    Concatenating, merging, filtering, or performing groupby operations on Pandas:
-    DataFrames:
-    Visualizing data with pyplot or seaborn:
+class Event:
+To create the event and get the specifics for the student group
 
-class Terp_Plans
-    Attributes:
-        name()
-        budget()
-        location() – can have a list and the user has to pick a location from that list
-        date()
-        time()
-        food()
-    Methods:
-        __init__()
-        budget_tracker(float)
-        create_event
-        event_approval
-        prices
-        find_food
+Init method
+Establish the location
+The length of the event calculations maybe? - hourly pay for the location 
+Food boolean optional parameter default false - optional parameter 
+
+Location checker
+Confirms if the location is on campus
+Will have the with statement
+Ask the user on the budget for location
+Create a List comprehension that gives the options available based on the user’s budget.
+User then picks the location that suits them
+
+estimate()
+Only runs if the food attribute boolean is true
+Conditional expression if food= true
+Asks for an estimate of how much the food gonna cost 
+
+Budget tracker
+Placeholder for the different categories of the overall budget
+
+class Budget:
+To ask for and keep a track of the group’s budget, and make sure that they don’t go over budget 
+
+Init() method
+
+
+Sub magic() method
+Will call and subtract from the budget tracker method (potential super)
+
+Budget breakdown
+
+Main
+F-string that will give the student group the finalized event budget and summary for their event they planned 
+Argument parser
+sequence unpacking 
+
+
 
 main()
 ArgParser
