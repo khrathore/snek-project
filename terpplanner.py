0c554d86cf928760e0810e9436e70d57efdc0c54
khrathore <44534747+khrathore@users.noreply.github.com>
2022-11-18 16:51:29 -0500

    Khushboo docstrings

--- terpplanner.py
+++ terpplanner.py
@@ -1,35 +1,50 @@
 
 # Terp Planner: helps a student org plan their event on campus
+import sys
 
 class User:
+    """Defines a user object for the person planning the event
+    
+    Attributes:
+        fname(str): The first name of the person
+        lname(str): The last name of the person
+        email(str): The UMD email of the person
+        org(str): The organization the event is for
+        
+    Methods:
+        email_check(): Checks that the email is a UMD email
+        org_check(): Checks that the org is one registered with UMD
+    """
     
     def __init__(self, fname, lname, email, org):
-        '''
+        """
         Init method - Palrika
             Takes in 
                 name (first) and (last)
                 email
                 organization name
-        '''
+        """
         
     def email_check(self):
-        '''
-        Email_check: - Regex - Khushboo
-            check if it's a umd.edu email using a regex
-            if it isn't, throw an error
-        '''
+        """
+        Khushboo: Uses a regex expression to check whether the email provided is a umd one.
         
-    def org_check(self, org_name):
-        '''
+        Errors:
+            None: If the program does not find a match, it gets angee
+        """
+        
+    def org_check(self):
+        """
         Takes the org name
         with statement to read the file
         compares to make sure it's an active campus org, otherwises errors
         
         Args:
-        org_name(str): Name of organization
+            org_name(str): Name of organization
         
-        Returns: Boolean Value
-        '''
+        Returns: 
+            Boolean Value
+        """
 class Event:
     """ Plans the event for a student organization based on different categories.
     Attributes:
@@ -47,7 +62,7 @@ class Event:
         
     """
     def __init__(self, location, evlength, evbudget, food=False, equip=False, music=False, supplies=False):
-        ''' Initializes the Event Class. - Sandra
+        """ Initializes the Event Class. - Sandra
         Args:
             location(str): The location of the event.
             evlength(float): The length of the event.
@@ -60,6 +75,7 @@ class Event:
             default is False.
             supplies(bool):Determines whether the event will need supplies;
             default is False.
+            full_budget(list): A list or dictionary that holds the money per category
         Side effects:
             Creates the location, evlength, evbudget, food, equip, music, and 
             supplies attributes.
@@ -69,10 +85,10 @@ class Event:
             Establish the location
             The length of the event calculations maybe? - hourly pay for the location 
             Food boolean optional parameter default false - optional parameter 
-        '''
+        """
         
     def loc_checker(self, filepath):
-        ''' Determines the best location to hold an event based on a given budget. - Sandra
+        """Determines the best location to hold an event based on a given budget. - Sandra
         Args: 
             filepath(str):A path to a file of locations on campus.
         Return:
@@ -83,10 +99,10 @@ class Event:
         User then picks the location that suits them
         This method will showcase a List Comprehension that gives the location options available 
         based on a given budget.
-        '''
+        """
         
     def event_id(self, idset):
-        '''
+        """
             Give an rand event ID for each event, also add it to a set of IDS
             check intersection or sth of the new random number and the set of IDs
             if the event ID already exists, do another randomization
@@ -99,10 +115,10 @@ class Event:
         
         Returns: event_id(int)
             
-        '''
+        """
         
     def budget_tracker(self, budget):
-        '''
+        """
         Budget tracker - Conditional Expression - Palrika
             Placeholder for the different categories of the overall budget
             estimate()
@@ -110,35 +126,45 @@ class Event:
             Conditional expression if food= true
             Asks for an estimate of how much the food gonna cost
             if budget trackers becomes negative call fundraise  
-        '''
+        """
         
     def fundraise(self):
-       '''
+       """
         Fundraise - f-string - Kabindra
             If the budget tracker becomes negative print a statement that uses f-strings to say "this is how much you need"
-        '''
+        """
+        
+    def bud_vis(self):
+        """
+        Khushboo: pyplot usage, creates a diagram of the budget distribution     
+        returns: a graph
+
+        """
 
 class Budget:
+    """Tracks budget objects
+    
+    Attributes:
+        type(str): The category of the budget
+        money(float): The budgeted amount for that category
+        
+    Methods:
+        bud_vis(): pyplot visualizaton of the budget distribution
+    """
     
     def __init__(self, type, money):
-        '''
+        """
         Init() method - Khushboo
-        '''
+        """
     def __sub__(self, other):
-        '''
+        """
         Sub magic() method - Magic Method - Palrika
             Error check for sub to check that they are two budget objects
             Will call and subtract from the budget tracker method
-        '''
-    
-    def bud_vis(self):
-        '''     
-            Budget_chart - Khushboo - Pyplot
-                Create a plot of the money allocation
-        '''
+        """
 
 def main(fname, lname, email, orgname):
-    ''' Calculates the overall event budget based on user input and then writes the completed
+    """ Calculates the overall event budget based on user input and then writes the completed
     event to a different file. -Sandra
     Args:
         fname(str): The user's first name.
@@ -148,7 +174,7 @@ def main(fname, lname, email, orgname):
     loop
         write to a doc the info of the event (including budget)
     Do you want to plan another event? if yes, restart loop
-    '''
+    """
 
 def parse_args(comline):
     """
