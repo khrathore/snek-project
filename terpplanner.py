43df7ab8503e59c687d46a15975c5508993014d2
Sandra Aching <sakathia01@gmail.com>
2022-11-18 00:22:29 -0500

    Update terpplanner.py

--- terpplanner.py
+++ terpplanner.py
@@ -27,23 +27,58 @@ class User:
             compares to make sure it's an active campus org, otherwises errors
         '''
 class Event:
-    
-    def __init__(self, location, evlength, evbudget, food=False, equip=False, music=False, decor=False):
-        '''
-        Init method - Optional Param - Sandra
+    """ Plans the event for a student organization based on different categories.
+    Attributes:
+        location(str): The location of the event.
+        evlength(float): The length of the event.
+        evbudget(float): The budget of the event as specified by the user.
+        food(bool): Determines whether the event will include food; default
+        is False.
+        equip(bool): Determines whether the event will need equipment;
+        default is False.
+        music(bool):Determines whether the event will need music;
+        default is False.
+        supplies(bool):Determines whether the event will need supplies;
+        default is False.
+        
+    """
+    def __init__(self, location, evlength, evbudget, food=False, equip=False, music=False, supplies=False):
+        ''' Initializes the Event Class. - Sandra
+        Args:
+            location(str): The location of the event.
+            evlength(float): The length of the event.
+            evbudget(float): The budget of the event as specified by the user.
+            food(bool): Determines whether the event will include food; default
+            is False.
+            equip(bool): Determines whether the event will need equipment;
+            default is False.
+            music(bool):Determines whether the event will need music;
+            default is False.
+            supplies(bool):Determines whether the event will need supplies;
+            default is False.
+        Side effects:
+            Creates the location, evlength, evbudget, food, equip, music, and 
+            supplies attributes.
+        This method will showcase the  Optional Parameter that will be used to 
+        determine the budgets for the different event budget categories. 
+            
             Establish the location
             The length of the event calculations maybe? - hourly pay for the location 
             Food boolean optional parameter default false - optional parameter 
         '''
         
-    def loc_checker(self):
-        '''
-        Location checker - List Comprehension - Sandra
-            Confirms if the location is on campus
-            Will have the with statement
-            Ask the user on the budget for location
-            Create a List comprehension that gives the options available based on the user’s budget.
-            User then picks the location that suits them
+    def loc_checker(self, filepath):
+        ''' Determines the best location to hold an event based on a given budget. - Sandra
+        Args: 
+            filepath(str):A path to a file of locations on campus.
+        Return:
+            str: The specific location that is within the given budget
+        Confirms if the location is on campus
+        Will have the with statement
+        Ask the user on the budget for location
+        User then picks the location that suits them
+        This method will showcase a List Comprehension that gives the location options available 
+        based on a given budget.
         '''
         
     def event_id(self, idset):
@@ -91,8 +126,13 @@ class Budget:
         '''
 
 def main(fname, lname, email, orgname):
-    # SANDRA Code that runs the function and takes user prompts and writes to a different file
-    '''
+    ''' Calculates the overall event budget based on user input and then writes the completed
+    event to a different file. -Sandra
+    Args:
+        fname(str): The user's first name.
+        lname(str): The user's last name.
+        email(str): The user's email.
+        orgname(str):The organization the user is associated with.
     loop
         write to a doc the info of the event (including budget)
     Do you want to plan another event? if yes, restart loop
