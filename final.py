dcbc8d6a7724e9f1383de416ae560ea99d82b583
Sandra Aching <sakathia01@gmail.com>
2022-12-12 15:32:40 -0500

    Update final.py

--- final.py
+++ final.py
@@ -19,6 +19,15 @@ class User:
 
 class Event:
     
+    def __init__(self, name, budget_obj, food_obj, equip_obj, supplies_obj, location_obj, duration):
+        self.name = name
+        self.budget_obj = budget_obj
+        self.food_obj = food_obj
+        self.equip_obj = equip_obj
+        self.supplies_obj = supplies_obj
+        self.location_obj = location_obj
+        self.duration = duration
+    
     def budget_tracker(self):
         self.budget_obj.amount = self.budget_obj - self.food_obj 
         self.budget_obj.amount = self.budget_obj - self.equip_obj
@@ -36,3 +45,23 @@ class Budget:
     def __sub__(self, other):
         check = self.amount - other.amount
         return check
+    
+    
+    
+    
+    
+      
+    
+def parse_args(comline):    
+    parser = ArgumentParser()
+    parser.add_argument("fname", help="first name of the student")
+    parser.add_argument("lname", help = "last name of the student")
+    parser.add_argument("email", help = "email of the student")
+    parser.add_argument("orgname", help = "name of the organization")
+
+    return parser.parse_args(comline)
+
+  
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    main(args.fname, args.lname, args.email, args.orgname)
